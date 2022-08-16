93e0634db0d3e5e108ca9e166defc47b2ebb20ab
Rifqi Aufan <rifqiaufan@gmail.com>
2022-08-16 15:49:53 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+from cv2 import CAP_PROP_PVAPI_BINNINGX
+from manual_crop import manual_crop
+
+
+path = "./data/ecg_test.JPG"
+im = cv2.imread(path)
+im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+# coordinate start from top left (0,0)
+cropping = False
+x_start, y_start, x_end, y_end = 0, 0, 0, 0
+
+param = [im, cropping]
+
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", manual_crop,param)
+
+# drawing rectangle
+i = im.copy()
+if not cropping:
+    cv2.imshow("image", i)
+
+elif cropping:
+    cv2.rectangle(i, (x_start, y_start), (x_end, y_end),(0,255,0),5)
+    cv2.imshow("image",i)
+
+cv2.waitKey(0)
+print(i.shape)
+
+
+
+
+
---
+++ manual_crop.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+def manual_crop(event,x,y,flags,param):
+    global x_start, y_start, x_end, y_end, cropping
+
+    im = param[0]
+    cropping = param[1]
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x_start, y_start, x_end, y_end = x,y,x,y
+        cropping = True
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if cropping == True:
+            x_end, y_end = x, y
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        x_end,y_end = x,y
+        cropping = False
+
+        refPoint = [(x_start,y_start),(x_end,y_end)]
+
+        if len(refPoint) == 2:
+            roi = im[refPoint[0][1]:refPoint[1][1],refPoint[0][0]:refPoint[1][0]]
+            cv2.imshow("Cropped",roi)
